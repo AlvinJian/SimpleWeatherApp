c4fbcd3340cdc20fc4dae54c56ff4ed00bbc6c45
alvin_chien <alvinchien0624@gmail.com>
2018-11-03 16:24:47 -0400

    refactor the code
    move getCurrentWeather down to child component

--- WeatherApp/ClientApp/src/App.js
+++ WeatherApp/ClientApp/src/App.js
@@ -8,11 +8,9 @@ export default class App extends Component {
         this.state = {
             paramType: OWMInputTypes.GeoLocation,
             paramVal: "",
-            weatherData: {}
         };
 
         this.getLocation = this.getLocation.bind(this);
-        this.getCurrentWeather = this.getCurrentWeather.bind(this);
 
         this.getLocation();
     }
@@ -25,49 +23,23 @@ export default class App extends Component {
                 this.setState({
                     paramType: this.state.paramType,
                     paramVal: "" + lat + "," + lon,
-                    weatherData: {}
                 })
+                console.log("lat,lon=" + this.state.paramVal);
             });
         } else {
             console.log("Geolocation is not supported by this browser.");
         }
     }
 
-    getCurrentWeather() {
-        var call = 'api/OWMReq/';
-        if (this.state.paramType === OWMInputTypes.GeoLocation) {
-            call += ('WeatherByGeo/' + this.state.paramVal);
-        }
-        else {
-            console.log("parameter type: " + this.state.paramType +
-                " is not supported");
-        }
-        console.log("call: " + call);
-        if (call.length > 0) {
-            fetch(call).then(response => response.json())
-                .then(data => {
-                    console.log(JSON.stringify(data));
-                    this.setState({
-                        paramType: this.state.paramType,
-                        paramVal: this.state.paramVal,
-                        weatherData: data
-                    });
-                })
-        }
-    }
+    
 
     render() {
-        if (this.state.paramVal.length > 1) {
-            this.getCurrentWeather();
-        }
-
         return (
             <div>
                 <h3>Simple Weather App</h3>
                 <CurrentWeather
                     paramType={this.state.paramType}
-                    paramVal={this.state.paramVal}
-                    weatherData={this.state.weatherData} />
+                    paramVal={this.state.paramVal} />
             </div>
         );
     }
--- WeatherApp/ClientApp/src/components/CurrentWeather.js
+++ WeatherApp/ClientApp/src/components/CurrentWeather.js
@@ -4,26 +4,57 @@ import { OWMInputTypes } from '../Config';
 export class CurrentWeather extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            weatherData: {}
+        };
+        this.getCurrentWeather = this.getCurrentWeather.bind(this);
+
+        this.getCurrentWeather();
     }
 
-
+    getCurrentWeather() {
+        var call = 'api/OWMReq/';
+        if (this.props.paramType === OWMInputTypes.GeoLocation) {
+            call += ('WeatherByGeo/' + this.props.paramVal);
+        }
+        else {
+            console.log("parameter type: " + this.props.paramType +
+                " is not supported");
+        }
+        console.log("call: " + call);
+        if (this.props.paramVal.length > 1) {
+            fetch(call).then(response => response.json())
+                .then(data => {
+                    console.log(JSON.stringify(data));
+                    this.setState({ weatherData: data });
+                })
+        }
+        else {
+            console.log("skip fetch");
+        }
+    }
 
     render() {
-        if (this.props.weatherData.code &&
-            this.props.weatherData.code === 'GOOD') {
+        console.log("CurWeather render");
+        if (this.state.weatherData.code &&
+            this.state.weatherData.code === 'GOOD') {
+            console.log("CurWeather is rendering")
             return (
                 <div>
                     <h1>
-                        {this.props.weatherData.city},
-                        {this.props.weatherData.country}
+                        {this.state.weatherData.city},
+                        {this.state.weatherData.country}
                     </h1>
-                    <h2> {this.props.weatherData.weather} </h2>
-                    <h2> {this.props.weatherData.temperature} F</h2>
-                    <h2> {this.props.weatherData.humidity} % </h2>
+                    <h2> {this.state.weatherData.weather} </h2>
+                    <h2> {this.state.weatherData.temperature} F</h2>
+                    <h2> {this.state.weatherData.temperatureMax} F</h2>
+                    <h2> {this.state.weatherData.temperatureMin} F</h2>
+                    <h2> {this.state.weatherData.humidity} % </h2>
                 </div>
             );
         }
         else {
+            this.getCurrentWeather();
             return (
                 <div>
                     <h2> No data yet :( </h2>
